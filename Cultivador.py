1e479249297e77ec83c6d231d1cc1045499481e6
MichMP <158522214+Michmp12@users.noreply.github.com>
2024-03-06 16:06:44 -0500

    Update Cultivador.py

--- Cultivador.py
+++ Cultivador.py
@@ -7,16 +7,16 @@ if tipo_de_tuberculoInt ==1:
     for  i in range (3):
         ltsInt = int(input('Con litros gastastes hoy -> '))
         papaInt += ltsInt
-print('Los litros consumidos esta semana para la riega de la papita fueron: ',papaInt)        
+    print('Los litros consumidos esta semana para la riega de la papita fueron: ',papaInt)        
 
 if tipo_de_tuberculoInt ==2:
      for  i in range (2):
             ltsInt = int(input('Con litros gastastes hoy  -> '))
             yucaInt += ltsInt
-print('Los litros consumidos esta semana para la riega de la yuquita fueron: ',yucaInt)   
+     print('Los litros consumidos esta semana para la riega de la yuquita fueron: ',yucaInt)   
 
 if tipo_de_tuberculoInt == 3:
- for  i in range (2):
+    for  i in range (2):
         ltsInt = int(input('Con litros gastastes hoy  -> '))
         zanahoriaInt += ltsInt
-print('Los litros consumidos esta semana para la riega de la zanahorita fueron: ',zanahoriaInt)
+ print('Los litros consumidos esta semana para la riega de la zanahorita fueron: ',zanahoriaInt)
